ed32755dea8072b18ae2b2c31690be1e674c279b
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-04-04 11:29:43 +0800

    feat(BcryptService): with implementation details

--- src/common/bcrypt/bcrypt.service.ts
+++ src/common/bcrypt/bcrypt.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class BcryptService {}
+export class BcryptService {
+  SALT = 10;
+
+  async hash(data: string): Promise<string> {
+    return await bcrypt.hash(data, this.SALT);
+  }
+
+  async compare(data: string, encrypted: string): Promise<boolean> {
+    return await bcrypt.compare(data, encrypted);
+  }
+}
